9351c0c643312ece4c71c1f1a17005782d15d440
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2021-11-16 19:55:41 -0800

    testing statements done

--- src/Test1.java
+++ src/Test1.java
@@ -1,2 +1,26 @@
+import org.junit.Test;
+
 public class Test1 {
+    @Test
+    public void testIsPalindrome() {
+        int inputs[][] = {
+                {},
+                {3, 1, 3},
+                {1,1,1},
+                {1,2,3,4},
+                {5,4,3,4,5},
+                {9,9,9,9,9,9,9,9,1}
+        };
+
+        boolean answers[][] = {
+                {false},
+                {true},
+                {true},
+                {false},
+                {true},
+                {false}
+        };
+
+
+    }
 }

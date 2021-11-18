0ac7377491df7d3acfdaa02cad371f50a94af3c7
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2021-11-17 20:01:29 -0800

    commit 5 (Integer converted to ArrayList)

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+
 public class Solution {
     public static boolean isPalindrome(int x){
+        boolean answer = false;
+        int number = x;
 
-        return false;
+        ArrayList<Integer> numberArray = new ArrayList<Integer>();
+
+        while(number > 0){
+            numberArray.add(number % 10);
+            number /= 10;
+        }
+
+
+
+        return answer;
     }
 }
--- src/Test1.java
+++ src/Test1.java
@@ -4,9 +4,9 @@ import static org.junit.Assert.assertEquals;
 public class Test1 {
     @Test
     public void testIsPalindrome() {
-        int inputs[] = {0,313,111,1234,54345,999,988918};
+        int inputs[] = {313,111,1234,54345,999,988918, 1211, 38563};
 
-        boolean answers[] = {true, true, true, false, true, true, false};
+        boolean answers[] = {true, true, false, true, true, false, false, false};
 
         for(int i = 0; i < inputs.length; i++){
             boolean actual = Solution.isPalindrome(inputs[i]);
